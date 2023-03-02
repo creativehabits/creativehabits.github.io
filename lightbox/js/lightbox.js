e38395bb4be4dbbc205f97a813171ba9b0a2669a
creativehabits <jamir_0423@hotmail.com>
2023-03-01 22:44:34 -0500

    lightbox

---
+++ lightbox/js/lightbox.js
@@ -0,0 +1,20 @@
+let lightboxContainer = document.getElementById("lightboxContainer");
+let lightboxBackground = document.getElementById("lightboxBackground");
+let lightbox = document.getElementById("lightbox");
+let lightboxCloser = document.getElementById("lightboxCloser");
+
+function openLightbox() {
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let coffeeImage = document.getElementById("coffeeImage");
+coffeeImage.onclick = openLightbox;
+
+function closeLightbox() {
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
